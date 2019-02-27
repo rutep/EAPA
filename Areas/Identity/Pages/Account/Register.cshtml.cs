58089509bee513a237c17435099ff0ca8d4ae110
rafno <rafnar1993@gmail.com>
2019-02-27 21:20:10 +0000

    Helgi has to fix merge

--- Areas/Identity/Pages/Account/Register.cshtml.cs
+++ Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -116,7 +116,9 @@ namespace webApi.Areas.Identity.Pages.Account
             returnUrl = returnUrl ?? Url.Content("~/");
             if (ModelState.IsValid)
             {
-                var user = new MyUser { UserName = Input.Email, Email = Input.Email };
+                var user = new MyUser { UserName = Input.Email, Email = Input.Email, other_email = Input.other_email,
+                PhoneNumber = Input.phone, address = Input.address, address2 = Input.address2, affiliation = Input.affiliation,
+                 postcode = Input.postcode, region = Input.region, city = Input.city, country = Input.country };
                 var result = await _userManager.CreateAsync(user, Input.Password);
                 if (result.Succeeded)
                 {
