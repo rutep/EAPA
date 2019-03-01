8229bcb091eeea794ebd63318e6407a6d2155e49
helgigretar <helgigretargunnars.96@gmail.com>
2019-03-01 14:14:28 +0000

    er að fara á nýtt branch koma öllu í master

--- Areas/Identity/Pages/Account/Admin/ManageAdminPages.cs
+++ Areas/Identity/Pages/Account/Admin/ManageAdminPages.cs
@@ -9,11 +9,13 @@ namespace webApi.Areas.Identity.Pages.Account.Admin
     public class ManageAdminPages
     {
         public static string ChangePassword => "ChangePassword";
+        public static string ManageUsers => "ManageUsers";
         public static string Index => "Index";
 
         public static string IndexNavClass(ViewContext viewContext) => PageAdminClass(viewContext, Index);
 
         public static string ChangePasswordNavClass(ViewContext viewContext) => PageAdminClass(viewContext, ChangePassword);
+        public static string ManageUsersNavClass(ViewContext viewContext) => PageAdminClass(viewContext, ManageUsers);
         public static string PageAdminClass(ViewContext viewContext, string page)
         {
             var activePage = viewContext.ViewData["ActivePage"] as string
---
+++ Areas/Identity/Pages/Account/Admin/ManageUsers.cshtml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace webApi.Areas.Identity.Pages.Account.Admin
+{
+    public class ManageUsersModel : PageModel
+    {
+        public void OnGet()
+        {
+            using (var context = new YourContext())
+            {
+                return await UserManager.Users.ToListAsync();
+            }
+        }
+    }
+}
