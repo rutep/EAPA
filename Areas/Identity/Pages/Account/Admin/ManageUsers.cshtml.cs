2f269793aad7fc5a889409957868d59a4df148b7
helgigretar <helgigretargunnars.96@gmail.com>
2019-03-04 23:19:27 +0000

    náði Db connection loksins

--- Areas/Identity/Pages/Account/Admin/ManageUsers.cshtml.cs
+++ Areas/Identity/Pages/Account/Admin/ManageUsers.cshtml.cs
@@ -2,19 +2,37 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using webApi.Data;
 
 namespace webApi.Areas.Identity.Pages.Account.Admin
 {
     public class ManageUsersModel : PageModel
     {
-        public void OnGet()
+        private readonly UserManager<MyUser> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+
+        private UserManager<MyUser> manager;
+        public ManageUsersModel(UserManager<MyUser> userManager)
         {
-            using (var context = new YourContext())
+            _userManager = userManager;
+        }
+        public async Task<IActionResult> OnGetAsync()
+        {
+            // Find the users in that role
+            try
             {
-                return await UserManager.Users.ToListAsync();
+                var roleUsers = _userManager.Users.ToList();
+                var user = await _userManager.FindByEmailAsync("johndoe@email.com");
             }
+            catch
+            {
+                int a = 4;
+            }
+            return Page();
         }
     }
 }
