6323c9ca4b99d894e1231c17884964523761ac55
helgigretar <helgigretargunnars.96@gmail.com>
2019-03-26 14:09:35 +0000

    added couple of static html sides and 2 css files

---
+++ Pages/presidents_opinion.cshtml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace webApi.Pages
+{
+    [AllowAnonymous]
+    public class presidents_opinionModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+
+}
