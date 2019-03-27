28a275b44293cac0b089ce2cb4842d2ccd899bd2
helgigretar <helgigretargunnars.96@gmail.com>
2019-03-27 19:34:13 +0000

    database modified and frontpage done

--- Pages/News.cshtml.cs
+++ Pages/News.cshtml.cs
@@ -2,12 +2,14 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 
 namespace webApi.Pages
 {
+   [AllowAnonymous]
     public class NewsModel : PageModel
     {
         private readonly webApi.Data.ApplicationDbContext _context;
