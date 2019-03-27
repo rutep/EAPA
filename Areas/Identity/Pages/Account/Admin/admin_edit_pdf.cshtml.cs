86229f03cb599b1afc45389be3d2da26ca9e0d42
helgigretar <helgigretargunnars.96@gmail.com>
2019-03-27 15:27:35 +0000

    admin view langt komið mep breytingar á layout og rafnar slóðiner admin_edit_pdf

---
+++ Areas/Identity/Pages/Account/Admin/admin_edit_pdf.cshtml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using webApi.Data;
+
+namespace webApi.Areas.Identity.Pages.Account.Admin
+{
+    public class admin_edit_pdfModel : PageModel
+    {
+        private readonly UserManager<MyUser> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+        private UserManager<MyUser> manager;
+        //Here I will create the variables that goes in to the model
+        public List<MyUser> user { get; set; }
+        public admin_edit_pdfModel(UserManager<MyUser> userManager)
+        {
+            _userManager = userManager;
+        }
+        public async Task<IActionResult> OnGetAsync()
+        {
+            var audkenni = _userManager.GetUserId(User);
+            var userinn = await _userManager.FindByIdAsync(audkenni);
+            if (await _userManager.IsInRoleAsync(userinn, "Member"))
+            {
+                return Redirect("/");
+            }
+            //Else is for admin
+            else
+            {
+                user = _userManager.Users.ToList();
+                return Page();
+            }
+          
+        }
+    }
+}
