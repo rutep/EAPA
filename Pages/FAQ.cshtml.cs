d2ef961dee25dcc4e94696ab5155a7fea1198754
helgigretar <helgigretargunnars.96@gmail.com>
2019-03-27 21:49:49 +0000

    Added faq to users

---
+++ Pages/FAQ.cshtml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using FAQ.Data;
+using webApi.Data;
+
+namespace webApi.Pages
+{
+    public class FAQModel : PageModel
+    {
+        private readonly webApi.Data.ApplicationDbContext _context;
+        public List<FAQ.Data.FAQ> FAQS;
+        public FAQModel(webApi.Data.ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult OnGet()
+        {
+            FAQS = _context.Faq.Where(s => s.Id >= 0).ToList();  
+            return Page();
+        }
+
+        [BindProperty]
+        public FAQ.Data.FAQ FAQ { get; set; }
+    }
+}
