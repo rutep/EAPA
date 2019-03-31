06272446c32caac7485ca9ad3fb8f5338a7b1854
rutep <peturda3@gmail.com>
2019-03-31 09:26:52 +0000

    update

--- Areas/Identity/Pages/Account/Admin/Boardmember/Delete.cshtml.cs
+++ Areas/Identity/Pages/Account/Admin/Boardmember/Delete.cshtml.cs
@@ -5,37 +5,57 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
-using BoardMemberEntity.Data;
+using Event.Data;
 using webApi.Data;
+using System.IO;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Hosting.Server;
+using System.Runtime.InteropServices.ComTypes;
+using Microsoft.AspNetCore.Identity;
 
 namespace webApi.Pages.Boardmember
 {
     public class DeleteModel : PageModel
     {
-        private readonly webApi.Data.ApplicationDbContext _context;
+        private readonly ApplicationDbContext _context;
+        private readonly IHostingEnvironment he;
+        private readonly UserManager<MyUser> _userManager;
 
-        public DeleteModel(webApi.Data.ApplicationDbContext context)
+        public DeleteModel(webApi.Data.ApplicationDbContext context, IHostingEnvironment e, UserManager<MyUser> userManager)
         {
             _context = context;
+            he = e;
+            _userManager = userManager;
         }
 
         [BindProperty]
-        public BoardMember BoardMember { get; set; }
+        public BoardMemberEntity.Data.BoardMember BoardMember { get; set; }
 
         public async Task<IActionResult> OnGetAsync(int? id)
         {
-            if (id == null)
+            var audkenni = _userManager.GetUserId(User);
+            var userinn = await _userManager.FindByIdAsync(audkenni);
+            if (await _userManager.IsInRoleAsync(userinn, "Member"))
             {
-                return NotFound();
+                return Redirect("/");
             }
-
-            BoardMember = await _context.BoardMember.FirstOrDefaultAsync(m => m.Id == id);
-
-            if (BoardMember == null)
+            //Else is for admin
+            else
             {
-                return NotFound();
+                if (id == null)
+                {
+                    return NotFound();
+                }
+
+                BoardMember = await _context.BoardMember.FirstOrDefaultAsync(m => m.Id == id);
+
+                if (BoardMember == null)
+                {
+                    return NotFound();
+                }
+                return Page();
+
             }
-            return Page();
         }
 
         public async Task<IActionResult> OnPostAsync(int? id)
@@ -47,6 +67,12 @@ namespace webApi.Pages.Boardmember
 
             BoardMember = await _context.BoardMember.FindAsync(id);
 
+            if (BoardMember.Image != "")
+            {
+                var fileName = Path.Combine(he.WebRootPath + "\\images\\boardmembers", BoardMember.Image);
+                System.IO.File.Delete(fileName);
+            }
+
             if (BoardMember != null)
             {
                 _context.BoardMember.Remove(BoardMember);
@@ -55,5 +81,8 @@ namespace webApi.Pages.Boardmember
 
             return RedirectToPage("./Index");
         }
+
+
+
     }
 }
--- Areas/Identity/Pages/Account/Admin/Boardmember/Edit.cshtml.cs
+++ Areas/Identity/Pages/Account/Admin/Boardmember/Edit.cshtml.cs
@@ -1,49 +1,86 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
-using BoardMemberEntity.Data;
-using webApi.Data;
+using Microsoft.AspNetCore.Hosting;
+using System.IO;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
 
 namespace webApi.Pages.Boardmember
 {
     public class EditModel : PageModel
     {
         private readonly webApi.Data.ApplicationDbContext _context;
-
-        public EditModel(webApi.Data.ApplicationDbContext context)
+        private readonly IHostingEnvironment he;
+        private readonly UserManager<MyUser> _userManager;
+        public EditModel(webApi.Data.ApplicationDbContext context, IHostingEnvironment e, UserManager<MyUser> userManager)
         {
             _context = context;
+            he = e;
+            _userManager = userManager;
         }
 
         [BindProperty]
-        public BoardMember BoardMember { get; set; }
-
+        public BoardMemberEntity.Data.BoardMember BoardMember { get; set; }
         public async Task<IActionResult> OnGetAsync(int? id)
         {
-            if (id == null)
+            var audkenni = _userManager.GetUserId(User);
+            var userinn = await _userManager.FindByIdAsync(audkenni);
+            if (await _userManager.IsInRoleAsync(userinn, "Member"))
             {
-                return NotFound();
+                return Redirect("/");
             }
-
-            BoardMember = await _context.BoardMember.FirstOrDefaultAsync(m => m.Id == id);
-
-            if (BoardMember == null)
+            //Else is for admin
+            else
             {
-                return NotFound();
+                if (id == null)
+                {
+                    return NotFound();
+                }
+
+                BoardMember = await _context.BoardMember.FirstOrDefaultAsync(m => m.Id == id);
+
+                if (BoardMember == null)
+                {
+                    return NotFound();
+                }
+                return Page();
+
             }
-            return Page();
         }
 
-        public async Task<IActionResult> OnPostAsync()
+        public async Task<IActionResult> OnPostAsync(string files)
         {
             if (!ModelState.IsValid)
             {
                 return Page();
+            }
+
+            if (HttpContext.Request.Form.Files.Count > 0)
+            {
+                Random random = new System.Random();
+                int id = random.Next(0, 100000);
+                IFormFile file = HttpContext.Request.Form.Files[0];
+                var fileName = Path.Combine(he.WebRootPath + "\\images\\boardmembers", id + file.FileName);
+                BoardMember.Image = id + file.FileName;
+                using (var stream = new FileStream(fileName, FileMode.Create))
+                {
+                    file.CopyTo(stream);
+                }
+                var original_data = _context.BoardMember.AsNoTracking().Where(P => P.Id == BoardMember.Id).FirstOrDefault();
+
+                if (original_data.Image != "")
+                {
+                    var fileNam = Path.Combine(he.WebRootPath + "\\images\\boardmembers", original_data.Image);
+                    System.IO.File.Delete(fileNam);
+                }
+            }
+            else
+            {
+                BoardMember.Image = "";
             }
 
             _context.Attach(BoardMember).State = EntityState.Modified;
